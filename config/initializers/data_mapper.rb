e7f3aa5647e8fc566bac30f0d1ab6ebef132a261
Steve Hayes <steve.hayes@cogentconsulting.com.au>
2009-09-26 11:40:08 +1000

    look for heroku environment for database

--- config/initializers/data_mapper.rb
+++ config/initializers/data_mapper.rb
@@ -1,3 +1,7 @@
 require "dm-core"
-hash = YAML.load(File.new(Rails.root + "/config/database.yml"))
-DataMapper.setup(:default, hash[Rails.env])
+if ENV['DATABASE_URL']
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+else
+  hash = YAML.load(File.new(Rails.root + "/config/database.yml"))
+  DataMapper.setup(:default, hash[Rails.env])
+end
