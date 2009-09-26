69c6e1452d18823376380a8b853fde60d40d4308
Steve Hayes <steve.hayes@cogentconsulting.com.au>
2009-09-26 11:44:00 +1000

    look for heroku environment for database refactored

--- config/initializers/data_mapper.rb
+++ config/initializers/data_mapper.rb
@@ -1,7 +1,16 @@
 require "dm-core"
-if ENV['DATABASE_URL']
+
+def is_heroku?
+  !!ENV['DATABASE_URL']
+end
+
+def setup_from_heroku
   DataMapper.setup(:default, ENV['DATABASE_URL'])
-else
+end
+
+def setup_from_config
   hash = YAML.load(File.new(Rails.root + "/config/database.yml"))
   DataMapper.setup(:default, hash[Rails.env])
 end
+
+is_heroku? ? setup_from_heroku : setup_from_config
