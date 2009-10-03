916c56ce4dd2a63bad4cc8f6e990bf66e0b61c0d
Steve Hayes <steve.hayes@cogentconsulting.com.au>
2009-10-03 21:37:08 +1000

    factored out shared example group

--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -1,122 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/crud_controller_spec')
 
 describe EmployeesController do
 
-  integrate_views
-  
   before(:each) do
     @model_class = Employee
     @valid_attributes = {:first_name => 'Steve', :surname => 'Hayes'}
   end
-  
-  def valid_attributes
-    @valid_attributes
-  end
-  
-  def collection_sym
-    @model_class.to_s.pluralize.underscore.to_sym
-  end
-  
-  def instance_sym
-    @model_class.to_s.underscore.to_sym
-  end
-  
-  def clear_instances
-    instances.destroy!
-  end
-  
-  def leave_one_instance
-    clear_instances
-    make_new_instance
-  end
-  
-  def instances
-    @model_class.all
-  end
-  
-  def instance
-    @model_class.first
-  end
-  
-  def make_new_instance
-    @model_class.make
-  end
 
-  def cache_new_instance
-    @instance = make_new_instance
-  end
-  
-  def cached_instance
-    @instance || cache_new_instance
-  end
-  
-  def should_render(template)
-    response.should render_template(template)
-  end
-
-  def collection_var
-    assigns[collection_sym]
-  end
-  
-  def instance_var
-    assigns[instance_sym]
-  end
-
-  def check_index_displayed
-    response.should redirect_to("http://test.host/employees")
-  end
-  
-  def check_instance_count(count)
-    instances.should have(count).records
-  end
-  
-  def check_instance_attributes
-    instance.attributes.should == valid_attributes.merge(:id => instance.id)
-  end
-  
-  it "should retrieve all instances for index" do
-    leave_one_instance
-    get :index
-    should_render("index")
-    collection_var.should have(1).record
-  end
-  
-  it "should retrieve one instance for show" do
-    leave_one_instance
-    get :show, :id => instance.id
-    should_render("show")
-    instance_var.should == instance
-  end
-
-  it "should destroy one instance" do
-    leave_one_instance
-    delete :destroy, :id => instance.id
-    check_index_displayed
-    check_instance_count(0)
-  end
-  
-  it "should display new instance" do
-    cache_new_instance
-    stub(@model_class).new {cached_instance}
-    put :new
-    should_render("new")
-    instance_var.should == cached_instance
-  end
-  
-  it "should create new instance" do
-    clear_instances
-    post :create, instance_sym => valid_attributes
-    check_index_displayed
-    check_instance_count(1)
-    check_instance_attributes
-  end
-  
-  it "should update instance" do
-    leave_one_instance
-    put :update, :id => instance.id, instance_sym => valid_attributes
-    check_index_displayed
-    check_instance_count(1)
-    check_instance_attributes
-  end
+  it_should_behave_like "any crud controller"
   
 end
