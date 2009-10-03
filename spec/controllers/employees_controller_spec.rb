1f723b5d5e298ee167940f94de04edaa5d266eb2
Steve Hayes <steve.hayes@cogentconsulting.com.au>
2009-10-03 20:31:31 +1000

    cleaning up test code for reuse

--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -4,21 +4,52 @@ describe EmployeesController do
 
   integrate_views
   
+  before(:each) do
+    @model_class = Employee
+  end
+  
+  def collection_sym
+    @model_class.to_s.pluralize.underscore.to_sym
+  end
+  
+  def instance_sym
+    @model_class.to_s.underscore.to_sym
+  end
+  
+  def clear_instances
+    @model_class.all.destroy!
+  end
+  
+  def make_new_instance
+    @model_class.make
+  end
+  
+  def should_render(template)
+    response.should render_template(template)
+  end
+
+  def collection_var
+    assigns[collection_sym]
+  end
+  
+  def instance_var
+    assigns[instance_sym]
+  end
+  
   it "should retrieve all instances for index" do
-    Employee.all.destroy!
-    Employee.make
-    Employee.make
+    clear_instances
+    make_new_instance
     get :index
-    response.should render_template("index")
-    assigns[:employees].should have(2).records
+    should_render("index")
+    collection_var.should have(1).record
   end
   
   it "should retrieve one instance for show" do
-    Employee.all.destroy!
-    employee = Employee.make
+    clear_instances
+    employee = make_new_instance
     get :show, :id => employee.id
-    response.should render_template("show")
-    assigns[:employee].should == employee
+    should_render("show")
+    instance_var.should == employee
   end
 
   it "should destroy one instance" do
@@ -37,8 +68,25 @@ describe EmployeesController do
     assigns[:employee].should == employee
   end
   
-  # def new
-  # def create
+  it "should create new instance" do
+    Employee.all.destroy!
+    valid_attributes = {:first_name => 'Steve', :surname => 'Hayes'}
+    put :create, :employee => valid_attributes
+    response.should redirect_to("http://test.host/employees")
+    Employee.all.should have(1).record
+    Employee.first.attributes.should == valid_attributes.merge(:id => Employee.first.id)
+  end
+  
+  it "should update instance" do
+    Employee.all.destroy!
+    Employee.make
+    valid_attributes = {:first_name => 'Steve', :surname => 'Hayes'}
+    put :update, :id => Employee.first.id, :employee => valid_attributes
+    response.should redirect_to("http://test.host/employees")
+    Employee.all.should have(1).record
+    Employee.first.attributes.should == valid_attributes.merge(:id => Employee.first.id)
+  end
+  
   # def update
   
 end
