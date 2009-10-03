ee15ef83b48aac4555b78ddb591de15a43b0c4d8
Steve Hayes <steve.hayes@cogentconsulting.com.au>
2009-10-04 08:42:41 +1100

    made a small dsl

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,8 +5,4 @@ class Employee
    property :surname,     String, :nullable => false
    property :first_name,  String, :nullable => false
    
-   # def to_param
-   #  return self.id.to_s
-   # end
-   
 end
--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -3,10 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/crud_controller_spec')
 
 describe EmployeesController do
 
-  before(:each) do
-    @model_class = Employee
-    @valid_attributes = {:first_name => 'Steve', :surname => 'Hayes'}
-  end
+  model Employee, {:first_name => 'Steve', :surname => 'Hayes'}
 
   it_should_behave_like "any crud controller"
   
