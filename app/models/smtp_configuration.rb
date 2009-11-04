b494db6aacdc1d40adda0102e36caee00c6c1e8d
Steve Hayes <steve.hayes@cogentconsulting.com.au>
2009-11-04 21:30:14 +1100

    logging smtp configuration

--- app/models/smtp_configuration.rb
+++ app/models/smtp_configuration.rb
@@ -1,3 +1,4 @@
+puts 'loading smtp configuration'
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.sendgrid.net",
   :port => '25',
@@ -6,3 +7,6 @@ ActionMailer::Base.smtp_settings = {
   :user_name => "steve.hayes@cogentconsulting.com.au",
   :password => SystemSetting.smtp_password
 }
+require 'pp'
+pp ActionMailer::Base.smtp_settings
+puts 'finished loading smtp configuration'
