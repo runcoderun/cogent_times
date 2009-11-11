3125a8b01cbb8545c35a50bc0ce02256cdb35ea3
Steve Hayes <steve.hayes@cogentconsulting.com.au>
2009-11-12 00:22:07 +1100

    fixed date selection problem

--- app/controllers/utilisations_controller.rb
+++ app/controllers/utilisations_controller.rb
@@ -1,7 +1,7 @@
 class UtilisationsController < BillingsController
 
   def select_date_range
-    redirect_to utilisations_url(:start_date => start_date, :end_date => end_date)
+    redirect_to utilisations_url(:start_date => selected_start_date, :end_date => selected_end_date)
   end
   
   def pie_data
