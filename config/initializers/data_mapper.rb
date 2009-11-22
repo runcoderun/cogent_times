f1666d29fac41cae4ac67bd2ba709c7f2db5ce25
Jason Rudolph <github@jasonrudolph.com>
2009-11-22 12:07:53 -0500

    Use "postgres" for DataMapper and "postgresql" for RunCodeRun
    RunCodeRun uses the ActiveRecord notation of "postgresql" to specify
    Postgres as the database.

--- config/initializers/data_mapper.rb
+++ config/initializers/data_mapper.rb
@@ -10,7 +10,9 @@ end
 
 def setup_from_config
   hash = YAML.load(File.new(File.dirname(__FILE__) + "/../database.yml"))
-  DataMapper.setup(:default, hash[Rails.env].symbolize_keys)
+  hash[Rails.env].symbolize_keys!
+  hash[Rails.env][:adapter] = "postgres" if hash[Rails.env][:adapter] == "postgresql"
+  DataMapper.setup(:default, hash[Rails.env])
 end
 
 is_heroku? ? setup_from_heroku : setup_from_config
